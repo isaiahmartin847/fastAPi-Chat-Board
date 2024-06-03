fd32150fed773be9c0ea5c01899f94eafb53916c
isaiah <isaiahmartin847@gmail.com>
2024-06-03 17:46:45 -0600

    added the create and the delete for the chats

--- src/main.py
+++ src/main.py
@@ -1,29 +1,46 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
+chat_db = {
+    0 : {"text": "first chat", "user" : "isaiah"}
+}
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+
+class Chat(BaseModel):
+    text: str
+    user: str
+    id: int
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/chats")
+def show_chats() -> dict:
+    return chat_db
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
+@app.put("/chat/create")
+def create_chat(chat: Chat):
+    if chat.id in chat_db:
+          raise HTTPException(status_code=409, detail="chat id already exist you can not rewrite over a chat")
+    else:
+        chat_db[chat.id] = {"user": chat.user, "text": chat.text}
+        return {"user": chat.user, "text": chat.text, "text id": chat.id}
+    
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.delete("/chat/delete/{chat_id}")
+def delete_chat(chat_id: int):
+    if chat_id in chat_db:
+        print("item was in the db")
+        del chat_db[chat_id]
+    else:
+        raise HTTPException(status_code=204, detail=f"chat with id of {chat_id} unable to delete chat")
+
+    return {"id": chat_id}
+
 
 
