62be967da140973066a345275cccaa8cdd9e25de
isaiahmartin <isaiahmartin847@gmail.com>
2024-06-14 14:44:09 -0600

    now main.py will connect to my local mysql data base

--- src/main.py
+++ src/main.py
@@ -2,9 +2,28 @@ from fastapi import FastAPI
 from chat import router as chat_router 
 from users import router as user_router
 from fastapi.middleware.cors import CORSMiddleware
+import mysql.connector 
+from mysql.connector.errors import Error
+
 
 
 app = FastAPI()
+
+try: 
+    connection = mysql.connector.connect(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        password='Developer1*',
+        database='Lagoon_test'  # Replace with your database name or remove if not connecting to a specific database
+    )
+
+    if connection.is_connected():
+        print("Connected to MySQL database")
+
+
+except Error as e:
+    print(f"Error: {e}")
 
 app.add_middleware(
     CORSMiddleware,
