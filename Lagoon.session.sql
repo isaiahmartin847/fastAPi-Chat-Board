0a4179feeaa98a8c3d8753d66a30d22b8db7f498
isaiahmartin <isaiahmartin847@gmail.com>
2024-06-14 20:09:13 -0600

    added the conection to the mysql db

---
+++ Lagoon.session.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO users (username, name, password) VALUES ("chris", "Chris", "Willies")
+
+
+
+-- SELECT * FROM users
--- src/main.py
+++ src/main.py
@@ -1,29 +1,33 @@
-from fastapi import FastAPI
-from chat import router as chat_router 
-from users import router as user_router
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
-import mysql.connector 
-from mysql.connector.errors import Error
-
-
+from mysql.connector import connect, Error
+from pydantic import BaseModel
+from typing import List, Dict, Any
+from contextlib import contextmanager
 
 app = FastAPI()
 
-try: 
-    connection = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        user='root',
-        password='Developer1*',
-        database='Lagoon_test'  # Replace with your database name or remove if not connecting to a specific database
-    )
+class User(BaseModel):
+    username: str
+    name: str
+    password: str
 
-    if connection.is_connected():
-        print("Connected to MySQL database")
-
-
-except Error as e:
-    print(f"Error: {e}")
+@contextmanager
+def get_db_connection():
+    try:
+        mydb = connect(
+            host='127.0.0.1',
+            port=3306,
+            user='root',
+            password='Developer1*',
+            database='Lagoon_test'
+        )
+        if mydb.is_connected():
+            yield mydb
+    except Error as e:
+        print(f"Error: {e}")
+    finally:
+        mydb.close()
 
 app.add_middleware(
     CORSMiddleware,
@@ -33,6 +37,28 @@ app.add_middleware(
     allow_headers=["Content-Type"],
 )
 
+@app.get("/db")
+def get_users():
+    with get_db_connection() as mydb:
+        cursor = mydb.cursor()
+        cursor.execute("SELECT * FROM users")
+        result = cursor.fetchall()
+        data = transform_query_output(result)
+        cursor.close()  # Make sure to close the cursor
+        return {"users": data}
+
+def transform_query_output(query_output: List[tuple]) -> Dict[int, Dict[str, Any]]:
+    users = {}
+    for user in query_output:
+        user_id, username, name, password = user
+        users[user_id] = {"username": username, "name": name, "password": password}
+    return users
+
+# Include other routers
+from chat import router as chat_router
+from users import router as user_router
+
 app.include_router(chat_router, prefix="/chat")
 app.include_router(user_router, prefix="/user")
 
+# Run the app using `uvicorn filename:app --reload` to see the output
